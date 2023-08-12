a0e224a25e1f3a3585ef4554112f5fd50278e897
thmmahendra <thmmahendra@gmail.com>
2023-08-12 15:36:53 +0545

    Initial Commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
 
 import AOS from "aos";
-import "./node_modules/aos/dist/aos.css";
+import "/aos/dist/aos.css";
 
 // init AOS animation
 AOS.init({
